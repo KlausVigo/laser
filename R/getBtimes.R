8053e2fd1038bc86414acc2a83dd3d2f7ae37ee5
klash <klaus.schliep@gmail.com>
2016-11-22 10:29:15 -0500

    added argument tol to control tolerance in internally called function is.ultrametric

--- R/getBtimes.R
+++ R/getBtimes.R
@@ -1,5 +1,5 @@
 `getBtimes` <-
-function(file=NULL, string=NULL)
+function(file=NULL, string=NULL, tol=1e-5)
 {
   if (is.null(string) && !is.null(file))
     tree <- read.tree(file)
@@ -7,7 +7,7 @@ function(file=NULL, string=NULL)
     tree <- read.tree(text = paste(string))
   else 
     stop("you must enter a filename or a character string\n")    
-  if (!is.ultrametric(tree))
+  if (!is.ultrametric(tree, tol))
     stop("Tree is not ultrametric!");
   return(rev(sort(as.numeric(branching.times(tree)))));
 }
